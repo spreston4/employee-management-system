01b8fb009bd870744cd8c2a7a6f661672df29c4f
spreston <sam.preston11@gmail.com>
2021-08-01 15:23:51 -0500

    Add employee functional.

--- index.js
+++ index.js
@@ -122,7 +122,9 @@ const addDept = async () => {
 
     const newDept = await inquirer.prompt(questions.addDeptQuestions);
 
-    await query(`INSERT INTO department (name) VALUES (?)`, newDept.depName.trim());
+    await query(`
+    INSERT INTO department (name) 
+    VALUES (?)`, newDept.depName.trim());
     await viewAllDepts();
 };
 
@@ -135,10 +137,14 @@ const addRole = async () => {
     const newRole = await inquirer.prompt(questions.addRoleQuestions)
 
     // Determine department id
-    const deptQuery = await query(`SELECT id from department WHERE name = (?)`, newRole.roleDept);
+    const deptQuery = await query(`
+    SELECT id from department 
+    WHERE name = (?)`, newRole.roleDept);
     const deptId = deptQuery[0].id;
 
-    await query(`INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)`,[newRole.roleName, parseInt(newRole.roleSalary), deptId]);
+    await query(`
+    INSERT INTO roles (title, salary, department_id) 
+    VALUES (?, ?, ?)`, [newRole.roleName, parseInt(newRole.roleSalary), deptId]);
 
     viewAllRoles();
 };
@@ -152,13 +158,24 @@ const addEmp = async () => {
 
     const newEmp = await inquirer.prompt(questions.addEmpQuestions);
 
-    console.log(newEmp);
-
     // Determine role id
+    const roleQuery = await query(`
+    SELECT id from roles 
+    WHERE title = (?)`, newEmp.empRole);
+    const roleId = roleQuery[0].id;
 
     // Determine manager id
+    const managerName = newEmp.empManager.split(' ');
+    const managerQuery = await query(`
+    SELECT id from employee 
+    WHERE first_name = (?) AND last_name = (?);`, [managerName[0], managerName[1]]);
+    const managerId = managerQuery[0].id;
+   
+    await query(`
+    INSERT INTO employee (first_name, last_name, role_id, manager_id)
+    VALUES (?, ?, ?, ?)`, [newEmp.firstName, newEmp.lastName, roleId, managerId]);
 
-    await askTodo();
+    viewAllEmps();
 };
 
 // updateEmpt will allow the user to update the role of an existing employee
@@ -195,7 +212,7 @@ const displayTable = (data) => {
 const getDepts = async () => {
     
     // Query all departments
-    const allDepts = await query(`SELECT * FROM department;`);
+    const allDepts = await query(`SELECT id, name FROM department;`);
 
     // Populate deptArr
     for (const depts of allDepts) {
@@ -209,21 +226,20 @@ const getDepts = async () => {
 // getDepts will query all available roles, then populate questions.roleArr with results
 const getRoles = async () => {
 
-    const allRoles = await query(`SELECT * FROM roles;`);
+    const allRoles = await query(`SELECT id, title FROM roles;`);
 
     for (const roless of allRoles) {
         const role = {};
         role.id = roless.id;
         role.name = roless.title;
         questions.roleArr.push(role);
-        console.log(role);
     };
 };
 
 // getEmps will query all available employees, then populate questions.empArr with results
 const getEmps = async () => {
 
-    const allEmps  = await query(`SELECT * FROM employee;`);
+    const allEmps  = await query(`SELECT id, first_name, last_name FROM employee;`);
     
     for (const emps of allEmps) {
         const emp = {};
